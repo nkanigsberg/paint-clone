e3e6a3fb42471edc2075a6401fabe25d65c59c28
Nathan Kanigsberg <nathan@kanigsberg.com>
2021-01-21 22:06:18 -0500

    allow click and drag to place multiple cells

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
 // TODO
-  // clear individual cells instead of redrawing entire canvas - massive performance gain
-  // click and drag to place multiple cells (nice to have)
   // add play mode (vs. placement mode)
    // toggle button
 
@@ -23,6 +21,18 @@ class Cell {
   /** Toggle whether or not this cell is active */
   toggleActive() {
     this.active = !this.active;
+    this.draw();
+  }
+
+  /** Set this cell to active */
+  setActive() {
+    this.active = true;
+    this.draw();
+  }
+
+  /** Set this cell to inactive */
+  setInactive() {
+    this.active = false;
     this.draw();
   }
 
@@ -111,6 +121,12 @@ life.board = [];
 /** The previously hovered cell (so can disable hover) */
 life.previousHover = {};
 
+/** What type of cell to toggle on click and drag */
+life.dragType = 'active';
+
+/** True if mouse has been dragged */
+life.mouseDrag = false;
+
 /** Create 2D array and draw the board */
 life.initializeBoard = () => {
   for (let y = 0; y < life.NUM_ROWS; y++) {
@@ -160,13 +176,53 @@ life.mouseMoveHandler = e => {
 
 /** toggle whether cell is active on click */
 life.mouseDownHandler = e => {
-  // get cell position from click
+  // get cell position
+  const cellX = Math.floor((e.pageX - life.canvas.offsetLeft) / life.CELL_WIDTH);
+  const cellY = Math.floor((e.pageY - life.canvas.offsetTop) / life.CELL_HEIGHT);
+
+  // set type of cell to fill on drag depending on what is initially clicked
+  life.dragType = life.board[cellY][cellX].active ? 'inactive' : 'active';
+
+  // event listeners for mouse drag and mouse up
+  life.canvas.addEventListener("mousemove", life.mouseDragHandler);
+  life.canvas.addEventListener("mouseup", life.mouseUpHandler);
+}
+
+/** Toggle cells when clicking and dragging over them */
+life.mouseDragHandler = e => {
+  life.mouseDrag = true;
+  // get cell position
   const cellX = Math.floor((e.pageX - life.canvas.offsetLeft) / life.CELL_WIDTH);
   const cellY = Math.floor((e.pageY - life.canvas.offsetTop) / life.CELL_HEIGHT);
 
   // toggle cell active (only if clicking on a cell)
-  if (cellY < life.BOARD_HEIGHT / life.CELL_HEIGHT && cellX < life.BOARD_WIDTH / life.CELL_WIDTH)
-    life.board[cellY][cellX].toggleActive();
+  if (cellY < life.BOARD_HEIGHT / life.CELL_HEIGHT && cellX < life.BOARD_WIDTH / life.CELL_WIDTH) {
+    if (life.dragType === 'active') life.board[cellY][cellX].setActive();
+    else life.board[cellY][cellX].setInactive();
+  }
+}
+
+/** disable mouse drag event listener on mouse up */
+life.mouseUpHandler = e => {
+  // remove drag listener
+  life.canvas.removeEventListener('mousemove', life.mouseDragHandler);
+  
+  // if mouse hasn't been dragged (ie. on click), toggle clicked cell
+  if (!life.mouseDrag) {
+    // get cell position
+    const cellX = Math.floor((e.pageX - life.canvas.offsetLeft) / life.CELL_WIDTH);
+    const cellY = Math.floor((e.pageY - life.canvas.offsetTop) / life.CELL_HEIGHT);
+
+    // toggle cell active (only if clicking on a cell)
+    if (cellY < life.BOARD_HEIGHT / life.CELL_HEIGHT && cellX < life.BOARD_WIDTH / life.CELL_WIDTH)
+      life.board[cellY][cellX].toggleActive();
+  }
+
+  // set mouse drag back to false
+  life.mouseDrag = false;
+
+  // remove this event listner
+  life.canvas.removeEventListener('mouseup', life.mouseUpHandler);
 }
 
 /** clear hover states when leaving canvas */
