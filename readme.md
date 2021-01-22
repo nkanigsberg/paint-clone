53bfea883180ec1ba2a27f3f2e8c0d8d3f1d940f
Nathan Kanigsberg <nathan@kanigsberg.com>
2021-01-22 17:37:31 -0500

    remove unneeded code

--- readme.md
+++ readme.md
@@ -1,19 +1,3 @@
-<h1>Game of Life</h1>
-<p>Based on <a href="https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life">Conway's Game of Life</a></p>
-<h2>Rules</h2>
-<h3>Standard</h3>
-<h4>For a space that is populated</h4>
-<ul>
-  <li>Each cell with one or no neighbours dies, as if by solitude</li>
-  <li>Each cell with four or more neighbours dies, as if by overpopulation</li>
-  <li>Each cell with two or three neighbours survives</li>
-</ul>
-<h4>For a space that is empty or unpopulated</h4>
-<ul>
-  <li>Each cell with three neighbours becomes populated</li>
-</ul>
-<h3>High Life</h3>
-<ul>
-  <li>Same as standard, plus:</li>
-  <li>Each cell with six neighbours becomes populated - this allows for more complex, and <a href="https://www.conwaylife.com/wiki/Replicator">self-replicating</a> patterns</li>
-</ul>
+<h1>Paint App</h1>
+<p>Work in progess</p>
+<p>Built on <a href="https://github.com/nkanigsberg/game-of-life">Game of Life</a> code</p>
--- script.js
+++ script.js
@@ -1,24 +1,15 @@
-// TODO
-  // possible to not check EVERY cell on play?
-  // grid/cell size options
-  // option to reset to seed - so people won't lose what they started with
-  // variant select - different rulesets
-
 // ========================== Cell ======================== //
 
 /** Class representing a Cell */
 class Cell {
-  constructor(coordinates, neighbours = 0, active = false, hover = false) {
+  constructor(coordinates, active = false) {
     this.coordinates = coordinates;
-    this.neighbours = neighbours;
     this.active = active;
-    this.hover = hover;
   }
 
   styles = {
     empty: 'rgb(0, 0, 0)',
     active: 'rgb(255, 255, 255)',
-    hover: 'rgba(252, 186, 3, 0.25)',
   }
 
   /** Toggle whether or not this cell is active */
@@ -39,30 +30,6 @@ class Cell {
     this.draw();
   }
 
-  /** if this cell isn't already hovered, enable its hover state (and disable other cell hover states) */
-  enableHover() {
-    if (!this.hover) {
-      this.hover = true;
-      this.draw();
-      
-      // if another cell is currently hovered - disable its hover
-      if (life.previousHover.coordinates) {
-        const { y, x } = life.previousHover.coordinates;
-        life.board[y][x].disableHover();
-      }
-
-      // set this cell as the previously hovered cell
-      life.previousHover = this;
-    }
-  }
-
-  /** disable this cell's hover state */
-  disableHover() {
-    if (this.hover) {
-      this.hover = false;
-      this.draw();
-    }
-  }
 
   /** draw this cell on the canvas */
   draw() {
@@ -78,12 +45,6 @@ class Cell {
     } else {
       ctx.strokeRect(coordX, coordY, life.CELL_WIDTH, life.CELL_HEIGHT);
     }
-
-    if (this.hover) {
-      ctx.fillStyle = this.styles.hover;
-      ctx.fillRect(coordX, coordY, life.CELL_WIDTH, life.CELL_HEIGHT);
-      ctx.fillStyle = this.styles.empty;
-    } 
   }
 
   /** erase the cell from the canvas */
@@ -94,48 +55,6 @@ class Cell {
 
     ctx.clearRect(coordX, coordY, life.CELL_WIDTH, life.CELL_HEIGHT);
   }
-
-  /** simulate life for this cell depending on active ruleset */
-  simulate() {
-    switch (life.ruleVariant) {
-      case 'standard':
-        if (this.active && (this.neighbours <= 1 || this.neighbours >= 4)) {
-          this.active = false;
-        } else if (!this.active && this.neighbours === 3) {
-          this.active = true;
-        }
-        break;
-      case 'highLife':
-        if (this.active && (this.neighbours <= 1 || this.neighbours >= 4)) {
-          this.active = false;
-        } else if (!this.active && (this.neighbours === 3 || this.neighbours === 6)) {
-          this.active = true;
-        }
-        break;
-    }
-    this.draw();
-  }
-
-  /** Returns the number of neighbours this cell has */
-  numNeighbours() {
-    const { x: cellX, y: cellY } = this.coordinates;
-    let result = 0;
-
-    // loop through surrounding x and y coordinates and count active cells
-    for (let y = cellY - 1; y <= cellY + 1; y++) {
-      // if inside vertical boundaries
-      if (y >= 0 && y < life.NUM_ROWS) {
-        for (let x = cellX - 1; x <= cellX + 1; x++) {
-          // if inside horizontal boundaries, and not the central cell
-          if (x >= 0 && x < life.NUM_COLUMNS && (y !== cellY || x !== cellX)) {
-            if (life.board[y][x].active) result++;
-          }
-        }
-      }
-    }
-    return result;
-  }
-
 }
 
 
@@ -155,39 +74,17 @@ life.NUM_COLUMNS = life.BOARD_WIDTH / life.CELL_WIDTH;
 life.NUM_ROWS = life.BOARD_HEIGHT / life.CELL_HEIGHT;
 
 // html elements
-life.canvas = document.getElementById("lifeCanvas");
+life.canvas = document.getElementById("paintCanvas");
 life.ctx = life.canvas.getContext("2d");
-
-life.clearBtn = document.getElementById("clear");
-
-life.playBtn = document.getElementById("play");
-life.playMessage = document.getElementById("playMessage");
-
-life.speedSlider = document.getElementById("speed");
-life.speedMessage = document.getElementById("speedMessage");
-
-life.variantSelect = document.getElementById("variant");
 
 /** The 2D array representation of the game board */
 life.board = [];
-
-/** The turn timer, in ms */
-life.turnTimer = 500;
-
-/** True if simulation is playing, false by default */
-life.play = false;
-
-/** The previously hovered cell (so can disable hover) */
-life.previousHover = {};
 
 /** What type of cell to toggle on click and drag */
 life.dragType = 'active';
 
 /** True if mouse has been dragged */
 life.mouseDrag = false;
-
-/** The active ruleset selected by the user ('standard' by default) */
-life.ruleVariant = 'standard';
 
 
 /** Create 2D array and draw the board */
@@ -220,59 +117,11 @@ life.clearBoard = () => {
   life.ctx.clearRect(0, 0, life.canvas.width, life.canvas.height);
 }
 
-/** 
- * Simulate one turn
- * - Two loops - otherwise will be counting updated neighbours mid-loop
- *  - one for counting up neighbours
- *  - two for updating positions
- * */
-life.simulate = () => {
-  // Count neighbours for all cells
-  life.board.forEach(row => {
-    row.forEach(cell => {
-      cell.neighbours = cell.numNeighbours();
-    })
-  })
-  // update cells based on counted neighbours
-  life.board.forEach(row => {
-    row.forEach(cell => {
-      cell.simulate();
-    })
-  })
-}
 
-/** Set the text for speed message */
-life.setSpeed = () => {
-  const speed = life.speedSlider.valueAsNumber;
-  life.turnTimer = speed;
-  life.speedMessage.innerText = `${speed}ms`;
-}
-
-/** Start the game timer */
-life.startTimer = () => {
-  life.interval = setInterval(() => {
-    life.simulate();
-  }, life.turnTimer);
-}
-
-/** Stop the game timer */
-life.stopTimer = () => {
-  clearInterval(life.interval);
-}
 
 
 // ==================== Event Handlers ====================== //
 
-/** add hover effect when cursor over a cell */
-life.mouseMoveHandler = e => {
-  // get cell position
-  const cellX = Math.floor((e.pageX - life.canvas.offsetLeft) / life.CELL_WIDTH);
-  const cellY = Math.floor((e.pageY - life.canvas.offsetTop) / life.CELL_HEIGHT);
-
-  // enable hover only if hovering over a cell
-  if (cellY < life.BOARD_HEIGHT / life.CELL_HEIGHT && cellX < life.BOARD_WIDTH / life.CELL_WIDTH)
-    life.board[cellY][cellX].enableHover();
-}
 
 /** toggle whether cell is active on click */
 life.mouseDownHandler = e => {
@@ -327,55 +176,8 @@ life.mouseUpHandler = e => {
 
 /** clear hover states when leaving canvas */
 life.mouseLeaveHandler = () => {
-  const prevHoverCoords = life.previousHover.coordinates;
-  life.board[prevHoverCoords.y][prevHoverCoords.x].disableHover();
-
   life.canvas.removeEventListener('mousemove', life.mouseDragHandler);
 }
-
-/** clear board and canvas on click */
-life.clearBtnClickHandler = () => {
-  // loop through board and set all cells to inactive
-  life.board.forEach(row => {
-    row.forEach(cell => {
-      cell.active = false;
-    })
-  })
-  // draw the cleared board
-  life.drawBoard();
-}
-
-/** Toggle play/pause on simulation */
-life.playBtnClickHandler = () => {
-  // toggle play/pause
-  life.play = !life.play;
-
-  // set button & message text
-  life.playBtn.innerHTML = life.play ? '<i class="fas fa-pause"></i>' : '<i class="fas fa-play"></i>';
-  life.playMessage.innerText = life.play ? 'Playing' : 'Paused';
-
-  // start simulation on play, stop on pause
-  if (life.play) life.startTimer();
-  else clearInterval(life.interval);
-}
-
-/** Set simulation speed and slider text on move */
-life.speedSliderMoveHandler = e => {
-  life.setSpeed();
-
-  // if game is playing, restart the timer
-  if (life.play) {
-    life.stopTimer();
-    life.startTimer();
-  }
-}
-
-/** Change the rule variant on select */
-life.variantSelectChangeHandler = e => {
-  life.ruleVariant = e.target.value;
-}
-
-
 
 
 // ==================== Initialize =================== //
@@ -391,17 +193,6 @@ life.init = () => {
   life.canvas.addEventListener("mousemove", life.mouseMoveHandler);
   life.canvas.addEventListener("mousedown", life.mouseDownHandler);
   life.canvas.addEventListener("mouseleave", life.mouseLeaveHandler);
-
-  // button listeners
-  life.clearBtn.addEventListener("click", life.clearBtnClickHandler);
-  life.playBtn.addEventListener("click", life.playBtnClickHandler);
-
-  // speed slider listeners
-  life.speedSlider.addEventListener("mousemove", life.speedSliderMoveHandler);
-  life.setSpeed(); // initially set speed in case slider is not in default position
-
-  // variant select listener
-  life.variantSelect.addEventListener("change", life.variantSelectChangeHandler);
 }
 
 
