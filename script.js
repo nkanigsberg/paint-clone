ff421d8c3ad1db057670b5a388d5cbee6942e912
Nathan Kanigsberg <nathan@kanigsberg.com>
2021-01-22 18:04:06 -0500

    connect gaps when dragging

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+// TODO
+  // connect the dots
+
 // ========================== Cell ======================== //
 
 /** Class representing a Cell */
@@ -36,26 +39,26 @@ class Cell {
     // erase this cell before (re)drawing
     this.erase();
 
-    const ctx = life.ctx;
-    const coordX = this.coordinates.x * life.CELL_WIDTH;
-    const coordY = this.coordinates.y * life.CELL_HEIGHT;
+    const ctx = paint.ctx;
+    const coordX = this.coordinates.x * paint.CELL_WIDTH;
+    const coordY = this.coordinates.y * paint.CELL_HEIGHT;
 
     if (this.active) {
       ctx.fillStyle = this.styles.active;
-      ctx.fillRect(coordX, coordY, life.CELL_WIDTH, life.CELL_HEIGHT);
+      ctx.fillRect(coordX, coordY, paint.CELL_WIDTH, paint.CELL_HEIGHT);
     } else {
       ctx.fillStyle = this.styles.default;
-      ctx.fillRect(coordX, coordY, life.CELL_WIDTH, life.CELL_HEIGHT);
+      ctx.fillRect(coordX, coordY, paint.CELL_WIDTH, paint.CELL_HEIGHT);
     }
   }
 
   /** erase the cell from the canvas */
   erase() {
-    const ctx = life.ctx;
-    const coordX = this.coordinates.x * life.CELL_WIDTH;
-    const coordY = this.coordinates.y * life.CELL_HEIGHT;
+    const ctx = paint.ctx;
+    const coordX = this.coordinates.x * paint.CELL_WIDTH;
+    const coordY = this.coordinates.y * paint.CELL_HEIGHT;
 
-    ctx.clearRect(coordX, coordY, life.CELL_WIDTH, life.CELL_HEIGHT);
+    ctx.clearRect(coordX, coordY, paint.CELL_WIDTH, paint.CELL_HEIGHT);
   }
 }
 
@@ -65,49 +68,52 @@ class Cell {
 // ==================== Life ==================== //
 
 /** @namespace life */
-const life = {};
+const paint = {};
 
 // constants
-life.CELL_WIDTH = 2;
-life.CELL_HEIGHT = 2;
-life.BOARD_WIDTH = 800;
-life.BOARD_HEIGHT = 600;
-life.NUM_COLUMNS = life.BOARD_WIDTH / life.CELL_WIDTH;
-life.NUM_ROWS = life.BOARD_HEIGHT / life.CELL_HEIGHT;
+paint.CELL_WIDTH = 1;
+paint.CELL_HEIGHT = 1;
+paint.BOARD_WIDTH = 800;
+paint.BOARD_HEIGHT = 600;
+paint.NUM_COLUMNS = paint.BOARD_WIDTH / paint.CELL_WIDTH;
+paint.NUM_ROWS = paint.BOARD_HEIGHT / paint.CELL_HEIGHT;
 
 // html elements
-life.canvas = document.getElementById("paintCanvas");
-life.ctx = life.canvas.getContext("2d");
+paint.canvas = document.getElementById("paintCanvas");
+paint.ctx = paint.canvas.getContext("2d");
 
 /** The 2D array representation of the game board */
-life.board = [];
+paint.board = [];
 
 /** What type of cell to toggle on click and drag */
-life.dragType = 'active';
+paint.dragType = 'active';
 
 /** True if mouse has been dragged */
-life.mouseDrag = false;
+paint.mouseDrag = false;
+
+/** The last cell activated during click and drag (used to connect the dots at high speed) */
+paint.lastDraggedCell = null;
 
 
 /** Create 2D array and draw the board */
-life.initializeBoard = () => {
-  for (let y = 0; y < life.NUM_ROWS; y++) {
-    life.board.push([]);
-    for (let x = 0; x < life.NUM_COLUMNS; x++) {
-      life.board[y].push(new Cell({ y, x }));
+paint.initializeBoard = () => {
+  for (let y = 0; y < paint.NUM_ROWS; y++) {
+    paint.board.push([]);
+    for (let x = 0; x < paint.NUM_COLUMNS; x++) {
+      paint.board[y].push(new Cell({ y, x }));
     }
   }
 
-  life.drawBoard();
+  paint.drawBoard();
 }
 
 /** Draw the board on the canvas */
-life.drawBoard = () => {
+paint.drawBoard = () => {
   // clear canvas before each draw
-  life.clearBoard();
+  paint.clearBoard();
 
   // draw each cell
-  life.board.forEach(row => {
+  paint.board.forEach(row => {
     row.forEach(cell => {
       cell.draw();
     })
@@ -115,8 +121,8 @@ life.drawBoard = () => {
 }
 
 /** clear the the board */
-life.clearBoard = () => {
-  life.ctx.clearRect(0, 0, life.canvas.width, life.canvas.height);
+paint.clearBoard = () => {
+  paint.ctx.clearRect(0, 0, paint.canvas.width, paint.canvas.height);
 }
 
 
@@ -126,79 +132,97 @@ life.clearBoard = () => {
 
 
 /** toggle whether cell is active on click */
-life.mouseDownHandler = e => {
+paint.mouseDownHandler = e => {
   // get cell position
-  const cellX = Math.floor((e.pageX - life.canvas.offsetLeft) / life.CELL_WIDTH);
-  const cellY = Math.floor((e.pageY - life.canvas.offsetTop) / life.CELL_HEIGHT);
+  const cellX = Math.floor((e.pageX - paint.canvas.offsetLeft) / paint.CELL_WIDTH);
+  const cellY = Math.floor((e.pageY - paint.canvas.offsetTop) / paint.CELL_HEIGHT);
 
   // set type of cell to fill on drag depending on what is initially clicked
-  life.dragType = life.board[cellY][cellX].active ? 'inactive' : 'active';
+  paint.dragType = paint.board[cellY][cellX].active ? 'inactive' : 'active';
 
   // event listeners for mouse drag and mouse up
-  life.canvas.addEventListener("mousemove", life.mouseDragHandler);
-  life.canvas.addEventListener("mouseup", life.mouseUpHandler);
+  paint.canvas.addEventListener("mousemove", paint.mouseDragHandler);
+  paint.canvas.addEventListener("mouseup", paint.mouseUpHandler);
 }
 
 /** Toggle cells when clicking and dragging over them */
-life.mouseDragHandler = e => {
-  life.mouseDrag = true;
+paint.mouseDragHandler = e => {
+  paint.mouseDrag = true;
+
+  const posX = e.pageX - paint.canvas.offsetLeft;
+  const posY = e.pageY - paint.canvas.offsetTop;
+
   // get cell position
-  const cellX = Math.floor((e.pageX - life.canvas.offsetLeft) / life.CELL_WIDTH);
-  const cellY = Math.floor((e.pageY - life.canvas.offsetTop) / life.CELL_HEIGHT);
+  const cellX = Math.floor(posX / paint.CELL_WIDTH);
+  const cellY = Math.floor(posY / paint.CELL_HEIGHT);
 
   // toggle cell active (only if clicking on a cell)
-  if (cellY < life.BOARD_HEIGHT / life.CELL_HEIGHT && cellX < life.BOARD_WIDTH / life.CELL_WIDTH) {
-    if (life.dragType === 'active') life.board[cellY][cellX].setActive();
-    else life.board[cellY][cellX].setInactive();
+  if (cellY < paint.BOARD_HEIGHT / paint.CELL_HEIGHT && cellX < paint.BOARD_WIDTH / paint.CELL_WIDTH) {
+    if (paint.dragType === 'active') paint.board[cellY][cellX].setActive();
+    else paint.board[cellY][cellX].setInactive();
+  
+    // if distance between the last painted cell and this cell is greater than 1, fill in the gap
+    if (paint.lastDraggedCell && (Math.abs(paint.lastDraggedCell.coordinates.x - cellX) > 1 || Math.abs(paint.lastDraggedCell.coordinates.y - cellY) > 1)) {
+      // console.log('fill in the gap');
+      const ctx = paint.ctx;
+      ctx.beginPath();
+      ctx.moveTo(posX, posY);
+      ctx.lineTo(paint.lastDraggedCell.coordinates.x, paint.lastDraggedCell.coordinates.y);
+      ctx.stroke();
+    }
+    paint.lastDraggedCell = paint.board[cellY][cellX];
   }
 }
 
 /** disable mouse drag event listener on mouse up */
-life.mouseUpHandler = e => {
+paint.mouseUpHandler = e => {
   // remove drag listener
-  life.canvas.removeEventListener('mousemove', life.mouseDragHandler);
+  paint.canvas.removeEventListener('mousemove', paint.mouseDragHandler);
   
   // if mouse hasn't been dragged (ie. on click), toggle clicked cell
-  if (!life.mouseDrag) {
+  if (!paint.mouseDrag) {
     // get cell position
-    const cellX = Math.floor((e.pageX - life.canvas.offsetLeft) / life.CELL_WIDTH);
-    const cellY = Math.floor((e.pageY - life.canvas.offsetTop) / life.CELL_HEIGHT);
+    const cellX = Math.floor((e.pageX - paint.canvas.offsetLeft) / paint.CELL_WIDTH);
+    const cellY = Math.floor((e.pageY - paint.canvas.offsetTop) / paint.CELL_HEIGHT);
 
     // toggle cell active (only if clicking on a cell)
-    if (cellY < life.BOARD_HEIGHT / life.CELL_HEIGHT && cellX < life.BOARD_WIDTH / life.CELL_WIDTH)
-      life.board[cellY][cellX].toggleActive();
+    if (cellY < paint.BOARD_HEIGHT / paint.CELL_HEIGHT && cellX < paint.BOARD_WIDTH / paint.CELL_WIDTH)
+      paint.board[cellY][cellX].toggleActive();
   }
 
   // set mouse drag back to false
-  life.mouseDrag = false;
+  paint.mouseDrag = false;
+
+  paint.lastDraggedCell = null;
 
   // remove this event listner
-  life.canvas.removeEventListener('mouseup', life.mouseUpHandler);
+  paint.canvas.removeEventListener('mouseup', paint.mouseUpHandler);
 }
 
 /** clear hover states when leaving canvas */
-life.mouseLeaveHandler = () => {
-  life.canvas.removeEventListener('mousemove', life.mouseDragHandler);
+paint.mouseLeaveHandler = () => {
+  paint.canvas.removeEventListener('mousemove', paint.mouseDragHandler);
+  paint.lastDraggedCell = null;
 }
 
 
 // ==================== Initialize =================== //
-life.init = () => {
+paint.init = () => {
   // set html canvas dimensions
-  life.canvas.setAttribute('width', life.BOARD_WIDTH);
-  life.canvas.setAttribute('height', life.BOARD_HEIGHT);
+  paint.canvas.setAttribute('width', paint.BOARD_WIDTH);
+  paint.canvas.setAttribute('height', paint.BOARD_HEIGHT);
 
   // initialize and draw the board
-  life.initializeBoard();
+  paint.initializeBoard();
 
   // mouse listeners
-  life.canvas.addEventListener("mousemove", life.mouseMoveHandler);
-  life.canvas.addEventListener("mousedown", life.mouseDownHandler);
-  life.canvas.addEventListener("mouseleave", life.mouseLeaveHandler);
+  paint.canvas.addEventListener("mousemove", paint.mouseMoveHandler);
+  paint.canvas.addEventListener("mousedown", paint.mouseDownHandler);
+  paint.canvas.addEventListener("mouseleave", paint.mouseLeaveHandler);
 }
 
 
 // ==================== Document Ready ==================== //
 (() => {
-  life.init();
+  paint.init();
 })()
